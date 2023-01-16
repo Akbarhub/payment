df6e8169105f8ef13a845feb9e78b17438466d57
babaevakbars@gmail.com <Akbarjon@go.alif.academy>
2023-01-16 18:39:06 +0500

    внесён изменения в пакете main

--- cmd/transfer/main.go
+++ cmd/transfer/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"payment/pkg/bank/card"
+	"fmt"
+	"payment/pkg/bank/types"
+)
+
 func main() {
-	
+cards := []types.Card{
+	{
+		PAN:     "5000 0000 0000 0003",
+		Balance: 1000,
+		Active:  true,
+	},
+	{
+		PAN:     "5000 0000 0000 0004",
+		Balance: -1000,
+		Active:  true,
+	},
+	{
+		PAN:     "5000 0000 0000 0005",
+		Balance: 1000,
+		Active:  false,
+	},
+}
+
+fmt.Println(card.PaymentSource(cards))
+
 }
