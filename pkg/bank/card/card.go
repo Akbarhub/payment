dbdb8be74e3bc96a35522f60d8e87dc845118640
babaevakbars@gmail.com <Akbarjon@go.alif.academy>
2023-01-16 18:43:42 +0500

    внесён изменение в пакете card

---
+++ pkg/bank/card/card.go
@@ -0,0 +1,28 @@
+package card
+
+import (
+	"git/pkg/bank/types"
+)
+
+
+func PaymentSource(cards []types.Card) []types.PaymentSource{
+
+	paySource := make([]types.PaymentSource, 0, len(cards))
+
+	for _, card := range cards {
+		if !card.Active {
+			continue
+		}
+		if card.Balance <= 0 {
+			continue
+		}
+		
+		paySource = append(paySource, types.PaymentSource{
+			Type: "card",
+			Number: string(card.PAN),
+			Balance: types.Money(card.Balance),
+		})
+	}
+
+	return paySource
+}
